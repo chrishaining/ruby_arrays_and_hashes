b202bf1daabc202da900f417a6bcfdcafbf83eda
Chris Haining <chrishaining@yahoo.co.uk>
2019-07-31 17:45:11 +0100

    completed exercises b and c.

--- arrays_hashes_quiz_start_point/exercise_b_users.rb
+++ arrays_hashes_quiz_start_point/exercise_b_users.rb
@@ -55,12 +55,52 @@ users = {
 }
 
 # 1. Get Jonathan's Twitter handle (i.e. the string `"jonnyt"`)
+# p users["Jonathan"][:twitter] #expects jonnyt - yay
 # 2. Get Erik's hometown
+#p users["Erik"][:home_town] #expects Linlithgow, and that's what I get
 # 3. Get the array of Erik's lottery numbers
+#p users["Erik"][:lottery_numbers] #yay
 # 4. Get the type of Avril's pet Monty
+# p users["Avril"][:pets][0][:species] #expects snake - not working, maybe because it's in an array
 # 5. Get the smallest of Erik's lottery numbers
+# p users["Erik"][:lottery_numbers].min #expects 8 - yay!
 # 6. Return an array of Avril's lottery numbers that are even
+# p users["Avril"][:lottery_numbers].select { |lottery_number| lottery_number % 2 == 0}
 # 7. Erik is one lottery number short! Add the number `7` to be included in his lottery numbers
+# p users["Erik"][:lottery_numbers].push(7) # yay
 # 8. Change Erik's hometown to Edinburgh
+# users["Erik"][:home_town] = "Edinburgh"
+# p users["Erik"][:home_town]
 # 9. Add a pet dog to Erik called "Fluffy"
+# users["Erik"][:pets].push({:name =>"Fluffy", :species => "dog"})
+# p users["Erik"][:pets]
 # 10. Add another person to the users hash
+
+#not sure how to add to a hash
+
+#
+# users_2 = {"name" => "Sue"}
+# #print users_2
+#
+# super_hash = {
+#   users: users,
+#   users_2: users_2
+# }
+# p super_hash
+
+users_2 = {
+    "Mandy" =>
+      {
+      :twitter => "mando",
+      :lottery_numbers => [3, 2, 9, 3, 5, 2],
+      :home_town => "Bordeaux",
+      :pets =>
+        [
+          {
+            :name => "Ratty",
+            :species => "lizard"
+          }
+        ]}
+    }
+new_users=[users, users_2]
+p new_users
--- arrays_hashes_quiz_start_point/exercise_c_uk.rb
+++ arrays_hashes_quiz_start_point/exercise_c_uk.rb
@@ -16,7 +16,25 @@ united_kingdom = [
   }
 ]
 
-# 1. Change the capital of Wales from `"Swansea"` to `"Cardiff"`.
+# 1. Change the capital of Wales from `"Swansea"` to `"Cardiff
+# united_kingdom[1][:capital] = "Cardiff"
+# p united_kingdom
+
 # 2. Create a Hash for Northern Ireland and add it to the `united_kingdom` array (The capital is Belfast, and the population is 1,811,000).
+
+united_kingdom.push({
+  name: "Northern Ireland",
+  population: 1811000,
+  capital: "Belfast"
+})
+#p united_kingdom
+
 # 3. Use a loop to print the names of all the countries in the UK.
+#
 # 4. Use a loop to find the total population of the UK.
+total_population = 0
+
+for country in united_kingdom
+   total_population += country[:population]
+end
+p total_population
